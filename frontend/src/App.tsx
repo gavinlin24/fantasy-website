badc5e5c9ecb1404c1660731e65ba7eb11985023
gavinlin24 <gavinlin24@gmail.com>
2025-10-12 19:08:31 -0700

    Added temporary routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Leaderboard from "./components/Leaderboard/Leaderboard";
+import News from "./components/News/News";
 
 function App() {
-  return <Leaderboard />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/leaderboard" replace />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
