14e1375c584b92cf3eaacdb8fbe33789ee82cb74
gavinlin24 <gavinlin24@gmail.com>
2025-10-25 23:19:36 -0700

    Add railway files

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 LEAGUE_ID = os.getenv("LEAGUE_ID")
-YEAR = int(os.getenv("YEAR"))
+YEAR = 2026
 ESPN_S2 = os.getenv("ESPN_S2")
 SWID = os.getenv("SWID")
 ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS").split(",")
